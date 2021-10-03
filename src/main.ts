1441c76ab8bca158dac9477653bb901bccf4e8ef
Colin Bell <colin@dogoodercomics.co.uk>
2021-10-03 17:38:07 +0100

    a bit more output for the user

--- src/main.ts
+++ src/main.ts
@@ -11,5 +11,12 @@ if(!configPath || !outputDirectory){
 }
 
 for(const {name, content} of generateLinktreat(readJSONSync(configPath))){
+    console.log(`Generating ${name}...`)
     writeFileSync(resolve(process.cwd(), outputDirectory, name), content);
+    console.log(`${name} created at ${outputDirectory}/${name}`)
 };
+
+console.log('Your linktreat is ready!')
+console.log('Try the following:')
+console.log(`> cd ${outputDirectory}`)
+console.log("> npx serve")
